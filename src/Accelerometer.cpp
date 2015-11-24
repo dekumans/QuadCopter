62c8a1da76521d0e06774f8a1c04468767eab2d1
Michiel Kustermans <michiel.kustermans@altran.com>
2015-11-24 23:59:57 +0100

    Added own accelerometer implementation with basic filtering

---
+++ src/Accelerometer.cpp
@@ -0,0 +1,101 @@
+#include <Wire.h>
+
+#include "Arduino.h"
+#include "Accelerometer.h"
+
+static float Accel_MG_LSB = 0.002F;   // 1, 2, 4 or 12 mg per lsb
+
+void Accelerometer::write8(uint8_t address, uint8_t reg, uint8_t value)
+{
+    Wire.beginTransmission(address);
+    Wire.write(reg);
+    Wire.write(value);
+    Wire.endTransmission();
+}
+
+uint8_t Accelerometer::read8(uint8_t address, uint8_t reg)
+{
+    uint8_t value;
+
+    Wire.beginTransmission(address);
+    Wire.write(reg);
+    Wire.endTransmission();
+
+    Wire.requestFrom(address, (uint8_t)1);
+    value = Wire.read();
+    Wire.endTransmission();
+
+    return value;
+}
+
+uint8_t Accelerometer::initAccelerometer()
+{
+    // Enable I2C
+    Wire.begin();
+
+    // Enable the accelerometer (1.344kHz)
+    write8(ADDRESS_ACCEL, REGISTER_ACCEL_CTRL_REG1_A, 0x97);
+    // High resolution, +- 4g
+    write8(ADDRESS_ACCEL, REGISTER_ACCEL_CTRL_REG4_A, 0x18);
+
+    // LSM303DLHC has no WHOAMI register so read CTRL_REG1_A back to check
+    // if we are connected or not
+    uint8_t reg1_a = read8(ADDRESS_ACCEL, REGISTER_ACCEL_CTRL_REG1_A);
+    if (reg1_a != 0x97) {
+        return 0;
+    }
+    return 1;
+}
+
+void Accelerometer::readAccelerometer()
+{
+    // Read the accelerometer
+    Wire.beginTransmission((uint8_t)ADDRESS_ACCEL);
+    Wire.write(REGISTER_ACCEL_OUT_X_L_A | 0x80);
+    Wire.endTransmission();
+    Wire.requestFrom((uint8_t)ADDRESS_ACCEL, (uint8_t)6);
+
+    // Wait around until enough data is available
+    while (Wire.available() < 6);
+
+    uint8_t xlo = Wire.read();
+    uint8_t xhi = Wire.read();
+    uint8_t ylo = Wire.read();
+    uint8_t yhi = Wire.read();
+    uint8_t zlo = Wire.read();
+    uint8_t zhi = Wire.read();
+
+    // Shift values to create properly formed integer (low byte first)
+    accelData.x = (int16_t)(xlo | (xhi << 8)) >> 4;
+    accelData.y = (int16_t)(ylo | (yhi << 8)) >> 4;
+    accelData.z = (int16_t)(zlo | (zhi << 8)) >> 4;
+
+    // Substract calibration data
+    accelData.x -= calibrateData.x;
+    accelData.y -= calibrateData.y;
+    accelData.z -= calibrateData.z;
+}
+
+void Accelerometer::calibrateAccelerometer()
+{
+    delay(100);
+
+    int i;
+    int cX, cY, cZ;
+
+    cX = 0;
+    cY = 0;
+    cZ = 0;
+
+    for (i = 0; i < CALIBRATION_STEPS; i++) {
+        readAccelerometer();
+        cX += accelData.x;
+        cY += accelData.y;
+        cZ += accelData.z;
+        delay(5);
+    }
+
+    calibrateData.x = cX / CALIBRATION_STEPS;
+    calibrateData.y = cY / CALIBRATION_STEPS;
+    calibrateData.z = (cZ / CALIBRATION_STEPS) - 500;
+}
--- src/sketch.ino
+++ src/sketch.ino
@@ -1,54 +1,25 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>
-#include <Adafruit_BMP085_U.h>
-#include <Adafruit_L3GD20_U.h>
-#include <Adafruit_10DOF.h>
-#include <kalman.h>
-
 #include "Arduino.h"
 #include "Led.h"
 #include "BatteryMonitor.h"
+#include "Accelerometer.h"
 
-kalman_data pitch_data;
-kalman_data roll_data;
+#define FILTER_SIZE 25
 
-Adafruit_10DOF                dof   = Adafruit_10DOF();
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
-Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
-Adafruit_L3GD20_Unified       gyro = Adafruit_L3GD20_Unified(20);
-Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
+Accelerometer accel;
+
+AccelData filteredData;
+AccelData filter[FILTER_SIZE];
 
 void initSensors()
 {
-  if(!accel.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println(F("Ooops, no LSM303 (Accel) detected ... Check your wiring!"));
-    while(1);
-  }
+    if(!accel.initAccelerometer())
+    {
+        /* There was a problem detecting the LSM303 ... check your connections */
+        Serial.println(F("Ooops, no LSM303 (Accel) detected ... Check your wiring!"));
+        while(1);
+    }
 
-  if(!mag.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no LSM303 (Mag) detected ... Check your wiring!");
-    while(1);
-  }
-
-  gyro.enableAutoRange(true);
-  if(!gyro.begin())
-  {
-    /* There was a problem detecting the L3GD20 ... check your connections */
-    Serial.println("Ooops, no L3GD20 (Gyro) detected ... Check your wiring!");
-    while(1);
-  }
-
-  if(!bmp.begin())
-  {
-    /* There was a problem detecting the BMP180 ... check your connections */
-    Serial.println("Ooops, no BMP180 (Baro) detected ... Check your wiring!");
-    while(1);
-  }
+    accel.calibrateAccelerometer();
 }
 
 void printBatteryLevels() {
@@ -60,51 +31,66 @@ void printBatteryLevels() {
     Serial.println(levels[2], DEC);
 }
 
+void printAccelData() {
+    Serial.print("Ax: ");
+    Serial.print(accel.accelData.x, DEC);
+    Serial.print(" Ay: ");
+    Serial.print(accel.accelData.y, DEC);
+    Serial.print(" Az: ");
+    Serial.println(accel.accelData.z, DEC);
+}
+
+void printFilteredData() {
+    Serial.print("Fx: ");
+    Serial.print(filteredData.x, DEC);
+    Serial.print(" Fy: ");
+    Serial.print(filteredData.y, DEC);
+    Serial.print(" Fz: ");
+    Serial.println(filteredData.z, DEC);
+}
+
+void calculateFilteredData() {
+    filteredData.x = 0;
+    filteredData.y = 0;
+    filteredData.z = 0;
+
+    int j;
+    for (j = 0; j < FILTER_SIZE; j++) {
+        filteredData.x += filter[j].x;
+        filteredData.y += filter[j].y;
+        filteredData.z += filter[j].z;
+    }
+
+    filteredData.x /= FILTER_SIZE;
+    filteredData.y /= FILTER_SIZE;
+    filteredData.z /= FILTER_SIZE;
+}
+
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Hello");
+
     initBatteryMonitor();
     initLeds();
     initSensors();
-    kalman_init(&pitch_data);
-    kalman_init(&roll_data);
 }
 
+int i = 0;
 void loop()
 {
-    sensors_event_t accel_event;
-    sensors_event_t mag_event;
-    sensors_event_t gyro_event;
-    sensors_event_t bmp_event;
-    sensors_vec_t   orientation;
+    if (i == FILTER_SIZE) {
+        calculateFilteredData();
+        printFilteredData();
+        i = 0;
+    }
 
-	float acc_x, acc_y, acc_z;
-	float gyr_x, gyr_y, gyr_z;
-	float acc_pitch, acc_roll;
-	float pitch, roll;
+    accel.readAccelerometer();
+    filter[i].x = accel.accelData.x;
+    filter[i].y = accel.accelData.y;
+    filter[i].z = accel.accelData.z;
 
-    // Read sensor
-    accel.getEvent(&accel_event);
-    gyro.getEvent(&gyro_event);
-    // Calculate pitch and roll based only on acceleration.
-    acc_pitch = atan2(accel_event.acceleration.x, accel_event.acceleration.z);
-    acc_roll = atan2(accel_event.acceleration.y, accel_event.acceleration.z);
-    // Perform filtering
-    kalman_innovate(&pitch_data, acc_pitch, gyro_event.gyro.x);
-    kalman_innovate(&roll_data, acc_roll, gyro_event.gyro.y);
-    // The angle is stored in state 1
-    pitch = pitch_data.x1 * 180.0 / 3.14159;
-    roll = roll_data.x1 * 180.0 / 3.14159;
-
-    Serial.print("Pitch: ");
-    Serial.print(pitch, 2);
-    Serial.print(" Roll: ");
-    Serial.print(roll, 2);
-    Serial.write(27);
-    Serial.print("[H");
-
-    ledToggle(0);
-
-    delay(10);
+    i++;
+    delay(4);
 }
+
